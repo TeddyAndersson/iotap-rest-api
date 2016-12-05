08a5880aeadac962f4de78a7248fe43d993bf238
teddyandersson <teddyandersson95@gmail.com>
2016-12-05 14:29:38 +0100

    inital project file structure

---
+++ run.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from grandiosa_site import app
+import os
+
+#run app on correct port
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
